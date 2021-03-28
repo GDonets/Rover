9162f496286b8be15fd7a623d2697aa1d3aedb76
Gleb Donets <gleb.donets@outlook.com>
2021-03-28 23:06:09 +0200

    Added testing project

---
+++ RoverIntegrationTests/RoverScenesTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace RoverIntegrationTests
+{
+    public class RoverScenesTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
